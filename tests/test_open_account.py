82c5a285a6c8dfc94f0b7695a8b0c5467b45ff7f
RA3N <ra3n@example.com>
2025-12-01 10:13:47 +0100

    c

--- tests/test_open_account.py
+++ tests/test_open_account.py
@@ -13,7 +13,7 @@ class TestOpenAccount:
 
     def create_driver(self):
         options = Options()
-        options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
--- tests/test_update_contact.py
+++ tests/test_update_contact.py
@@ -13,7 +13,7 @@ class TestUpdateContactInfo:
 
     def create_driver(self):
         options = Options()
-        options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--start-maximized')
